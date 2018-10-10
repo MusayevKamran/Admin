e2a6680511c129080ea9b375dadf31e78f122731
Kamran C. Musayev <musayevk@modenis.local>
2018-10-10 17:52:55 +0400

    first commit

---
+++ Data/TutorialContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorial.Data
+{
+    public class TutorialContext : DbContext
+    {
+        public TutorialContext() {}
+        public TutorialContext(DbContextOptions<TutorialContext> options) : base(options) { }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TutorialDB;Trusted_Connection=True;");
+            }
+        }
+
+    }
+}
